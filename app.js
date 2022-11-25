d586a3dbcf8f780e943584b7fb815df29e987d11
Peadar Jolliffe-Byrne <peadarjb@gmail.com>
2022-11-24 20:31:12 -0600

    Connecting app to Google authorisation

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 // const bcrypt = require("bcrypt");
 // const saltRounds = 10;
 // const md5 = require("md5")
@@ -41,6 +43,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // Adding encryption to the password field in the userSchema
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
@@ -54,6 +57,21 @@ passport.use(new LocalStrategy(User.authenticate()));
 // use static serialize and deserialize of model for passport session support
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+    // authorizationURL: 'https://www.example.com/oauth2/authorize',
+    // tokenURL: 'https://www.example.com/oauth2/token',
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
   res.render("home");
